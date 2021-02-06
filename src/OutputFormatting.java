7c5ba3e7de71b313ed498d1302862e4f0a80362c
Phani-Devarapu <70983537+Phani-Devarapu@users.noreply.github.com>
2021-02-06 11:18:08 -0500

    Question Output Formatting

---
+++ src/OutputFormatting.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class OutputFormatting {
+	
+	//PROBLEM STATEMENT
+	
+	/*
+	 * Input Format
+
+	Every line of input will contain a String followed by an integer.
+	Each String will have a maximum of  alphabetic characters, and each integer will be in the inclusive range from  to .
+
+	Output Format
+
+	In each line of output there should be two columns:
+	The first column contains the String and is left justified using exactly  characters.
+	The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+	Sample Input
+
+	java 100
+	cpp 65
+	python 50
+	Sample Output
+
+	================================
+	java           100 
+	cpp            065 
+	python         050 
+	================================*/
+	
+	
+
+	public void outFormatter()
+	{
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("================================");
+	    for(int i=0;i<3;i++)
+	    {
+	        String s1=sc.next();
+	        int x=sc.nextInt();
+	        //Complete this line;
+	        String intFormatter = String.format("%03d",x);
+	        System.out.printf("%-15s",s1);
+	        System.out.print(intFormatter+"\n");
+	        
+	    }
+	    System.out.println("================================");
+	}
+
+
+}
